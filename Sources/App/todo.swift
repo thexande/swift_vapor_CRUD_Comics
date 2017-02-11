16c974718f5f6dd565c47aba74a5f734a6b11cf7
Alexander Murphy <alex.murphy@ibotta.com>
2017-02-10 20:43:00 -0700

    postgres added

---
+++ Sources/App/todo.swift
@@ -0,0 +1,17 @@
+//
+//  todo.swift
+//  todo_vapor_swift
+//
+//  Created by Alex Murphy on 2/10/17.
+//
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+    let content: String
+    let image_url: String
+    let author: String
+    
+}
