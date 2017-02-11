5e57426b5e51dc7dda1b6ca9d1e15ec6fb591591
Alexander Murphy <alex.murphy@ibotta.com>
2017-02-10 21:02:37 -0700

    postgres configured

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,6 +2,14 @@ import Vapor
 import VaporPostgreSQL
 
 let drop = Droplet()
+drop.preparations.append(Post.self)
+
+do {
+    try drop.addProvider(VaporPostgreSQL.Provider.self)
+} catch {
+    assertionFailure("Error adding provider: \(error)")
+}
+
 
 drop.get { req in
     return try drop.view.make("welcome", [
